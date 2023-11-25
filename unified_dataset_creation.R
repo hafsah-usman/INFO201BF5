71a48d0d750852aa6d694349a6d016499a798fcf
hafsah-usman <146280072+hafsah-usman@users.noreply.github.com>
2023-11-25 14:32:06 -0800

    Update unified_dataset_creation.R
    loading libraries, reading in datasets

--- unified_dataset_creation.R
+++ unified_dataset_creation.R
@@ -1,1 +1,7 @@
+library(dplyr)
+library(stringr)
+library(testthat)
+library(shiny)
 
+covid_df <- read.csv("WHO-COVID-19-global-data.csv") #WHO-COVID-19-global-data.csv
+mental_health_df <- read.csv("Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv")
